03d5fad097f43ffd6bba43f07fe3f61f74c8cc44
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2021-12-24 21:00:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-  setup()
+  setup(  name = 'pystacks',         
+  packages = ['pystacks'],   #
+  version = '0.1',   
+        download_url = 'https://github.com/kellantech/pystacks/archive/refs/tags/v0.1.tar.gz',
+)
