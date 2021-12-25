b6b76a7951c30c2b17d623c4531767e345d50ded
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2021-12-24 21:10:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,19 @@ if __name__ == '__main__':
   packages = ['pystacks'],   #
   version = '0.1',   
         download_url = 'https://github.com/kellantech/pystacks/archive/refs/tags/v0.1.tar.gz',
+          keywords = ['stacks', 'python', ':)'],   #
+  install_requires=[           
+         
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta ',      #
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+
 )
