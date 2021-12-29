29ccc0351c9b80d9fb8392ca6195cdfbc395031e
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2021-12-29 10:37:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-  setup(  name = 'pystacks',         
-  packages = ['pystacks'],   #
+  setup(  name = 'superstacks',         
+  packages = ['superstacks'],   #
   version = '0.1',   
         download_url = 'https://github.com/kellantech/pystacks/archive/refs/tags/v0.1.tar.gz',
           keywords = ['stacks', 'python', ':)'],   #
